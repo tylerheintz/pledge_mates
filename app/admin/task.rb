c61d99c60fd857c84c5f9623cc5d74658b69a183
Prelang Builder <builder@prelang.com>
2016-05-25 00:50:47 +0000

    Permitted paramaters (attributes) in ActiveAdmin configuration for 'Task' to allow for create, update

--- app/admin/task.rb
+++ app/admin/task.rb
@@ -1,4 +1,5 @@
 ActiveAdmin.register Task do
+  permit_params :name, :description, :due_date, :completed, :location, :price, :pledge_id
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
